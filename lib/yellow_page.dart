f01fca36f341ac7ab9cf5b267d95f7b173404e90
Burak Turker <pythonu0@gmail.com>
2024-02-23 17:07:25 +0300

    popUntil, PushNamedAndRemoveUntil

--- lib/yellow_page.dart
+++ lib/yellow_page.dart
@@ -6,10 +6,62 @@ class yellowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('yellowPage'),backgroundColor: Colors.yellow,),
+      appBar: AppBar(
+        title: Text('yellowPage'),
+        backgroundColor: Colors.yellow,
+      ),
       body: Center(
-        child: Text('yellowPage',style: TextStyle(fontSize: 24),),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).popUntil((route) => route.isFirst);//ilk sayfaya geri dön anlamına gelir
+              },
+              child: Text(
+                'İlk sayfaya Dön',
+                style: TextStyle(color: Colors.white),
+              ),
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
+            ),
+            ElevatedButton(
+              onPressed: () {
+              Navigator.of(context).popUntil((route) => route.settings.name =='/greenPage');//ortadaki elemana dönecek route adı  '/greenPage' olana kadar geri dön anlamına geliyor
+              },
+              child: Text(
+                'Ortadaki Sayfaya geri Dön',
+                style: TextStyle(color: Colors.white),
+              ),
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Colors.deepOrange),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).popUntil((route) => route.settings.name =='/');
+              },
+              child: Text(
+                'İlk sayfaya geri dön yol 2',
+                style: TextStyle(color: Colors.white),
+              ),
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                  Navigator.of(context).pushNamedAndRemoveUntil('/greenPage', (route) =>route.isFirst );
+                  //ilk sayfaya kadar git sonra oraya yeşil sayfayı ekle (Yeşil sayfayı açıyo yeşilden geriye gitmek istediginde ilk sayfaya atıyo)
+              },
+              child: Text(
+                'yeşil ama  Anasayfadan sonra',
+                style: TextStyle(color: Colors.white),
+              ),
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
