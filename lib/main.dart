b28d2ba136b9e7ff5a0ad46a2d1c2edcfffcd0a3
Burak Turker <pythonu0@gmail.com>
2024-02-23 10:12:19 +0300

    Gidilen sayfadan geri gelme ve sayfalar arasından veri gönderme

--- lib/main.dart
+++ lib/main.dart
@@ -17,33 +17,38 @@ class MyApp extends StatelessWidget {
 
 class AnaSayfa extends StatelessWidget {
   const AnaSayfa({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Navigasyon işlemleri'),
+      appBar: AppBar(
+        title: const Text('Navigasyon işlemleri'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  //İkiside Aynı
+                  //Navigator.push(context, route);
+                  int? gelenSayi = await Navigator.of(context).push<int>(
+                      MaterialPageRoute(builder: (RedContext) => RedPage()));
+                  debugPrint('AnaSayfa GelenSayi: $gelenSayi');
+                },
+                child: Text('Kirmizi Sayfaya git Android'),
+                style: ElevatedButton.styleFrom(primary: Colors.red.shade300)),
+            ElevatedButton(
+                onPressed: () async {
+                 int? gelenSayi  =await Navigator.push<int>(context,
+                      CupertinoPageRoute(builder: (RedContex) => RedPage())).then((int? value){
+                        debugPrint('AnaSayfa GelenSayi Ios: $value');
+                      });
+                },
+                child: Text('Kirmizi Sayfaya git IOS'),
+                style: ElevatedButton.styleFrom(primary: Colors.red.shade300)),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(onPressed: (){
-                //İkiside Aynı  
-                //Navigator.push(context, route);
-                Navigator.of(context).push(MaterialPageRoute(builder: (RedContext)=>RedPage()));
-              }, child: Text('Kirmizi Sayfaya git Android'),style: ElevatedButton.styleFrom(
-                primary: Colors.red.shade300
-              )),
-
-              ElevatedButton(onPressed: (){
-                Navigator.push(context, CupertinoPageRoute(builder: (RedContex)=>RedPage()));
-              }, child: Text('Kirmizi Sayfaya git IOS'),style: ElevatedButton.styleFrom(
-                primary: Colors.red.shade300
-              )),
-            ],
-          ),
-        ),
-      );
+      ),
+    );
   }
 }
--- lib/red_page.dart
+++ lib/red_page.dart
@@ -1,15 +1,37 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class RedPage extends StatelessWidget {
-  const RedPage({super.key});
+  RedPage({super.key});
+  int _rasgeleSayi = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('RedPage'),backgroundColor: Colors.red,),
+      appBar: AppBar(
+        title: Text('RedPage'), backgroundColor: Colors.red,
+        automaticallyImplyLeading:
+            false, //sayfanın sol üstündeki geri dön buttonunu aç kapa yapıyor
+      ),
       body: Center(
-        child: Text('RedPage',style: TextStyle(fontSize: 24),),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'RedPage',
+              style: TextStyle(fontSize: 24),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  _rasgeleSayi = Random().nextInt(100);
+                  print('Random Sayi $_rasgeleSayi');
+                  Navigator.of(context).pop(_rasgeleSayi);
+                },
+                child: Text(' Geri dön')),
+          ],
         ),
+      ),
     );
   }
 }
