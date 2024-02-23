743831fbf70d882c00267dac0f1a5c9b29be7974
Burak Turker <pythonu0@gmail.com>
2024-02-23 12:25:20 +0300

    push Replacement , Maybe pop,can pop

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_navigation/green_page.dart';
 import 'package:flutter_navigation/red_page.dart';
 
 void main() => runApp(const MyApp());
@@ -39,12 +40,37 @@ class AnaSayfa extends StatelessWidget {
                 style: ElevatedButton.styleFrom(primary: Colors.red.shade300)),
             ElevatedButton(
                 onPressed: () async {
-                 int? gelenSayi  =await Navigator.push<int>(context,
-                      CupertinoPageRoute(builder: (RedContex) => RedPage())).then((int? value){
-                        debugPrint('AnaSayfa GelenSayi Ios: $value');
-                      });
+                  int? gelenSayi = await Navigator.push<int>(context,
+                          CupertinoPageRoute(builder: (RedContex) => RedPage()))
+                      .then((int? value) {
+                    debugPrint('AnaSayfa GelenSayi Ios: $value');
+                  });
                 },
                 child: Text('Kirmizi Sayfaya git IOS'),
+                style: ElevatedButton.styleFrom(primary: Colors.red.shade300)),
+            ElevatedButton(
+                onPressed: () async {
+                  if (Navigator.canPop(context)) {
+                    debugPrint('evet pop olabilir');
+                  } else {
+                    debugPrint('Hayir pop olamaz');
+                  }
+                },
+                child: Text('Maybe Pop Kullanimi'),
+                style: ElevatedButton.styleFrom(primary: Colors.red.shade300)),
+            ElevatedButton(
+                onPressed: () async {
+                  Navigator.of(context)
+                      .maybePop(); //Öncesinde bir sayfa varsa oraya gitsin yoksa ilk sayfa ise bişe yapmasın
+                },
+                child: Text('maybe pop'),
+                style: ElevatedButton.styleFrom(primary: Colors.red.shade300)),
+            ElevatedButton(
+                onPressed: () async {
+                  // normal push ile aynı işlemi yapar farkı ise  gittiginiz siteyi ana sayfa yapar yani geriye döndügünüzde ilk sayfayı bulamazsınız direk  çıkış yaparsınız örnek (login sayfası giriş yaptıktan sonra anasayfaya gidiyo geriye bastıgında login sayfasına degil de uygulamadan çıkıyo)
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: ((context) => greenPage())));
+                },
+                child: Text('Push pushReplacement'),
                 style: ElevatedButton.styleFrom(primary: Colors.red.shade300)),
           ],
         ),
--- lib/red_page.dart
+++ lib/red_page.dart
@@ -8,29 +8,62 @@ class RedPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('RedPage'), backgroundColor: Colors.red,
-        automaticallyImplyLeading:
-            false, //sayfanın sol üstündeki geri dön buttonunu aç kapa yapıyor
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'RedPage',
-              style: TextStyle(fontSize: 24),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  _rasgeleSayi = Random().nextInt(100);
-                  print('Random Sayi $_rasgeleSayi');
-                  Navigator.of(context).pop(_rasgeleSayi);
-                },
-                child: Text(' Geri dön')),
-          ],
+    //will popScope kaldırılmış
+    /*return PopScope(
+      canPop: true, //canPop mobil cihazsın  automaticallyImplyLeading aççıkkenki dönüşünü donanımdaki dönüş buttonu dahil geri dönüş buttonlarını engelliyo
+      
+      onPopInvoked: (didPop){  
+        //debugPrint('Pop Scope Çalisti');        
+        // _rasgeleSayi = Random().nextInt(100);
+        // Navigator.pop(context,_rasgeleSayi);
+      },
+      */
+
+
+      //!!!! WillPopScope artık kullanılmıyo onun yerine "PopScope" Kullanılıyo 
+    return WillPopScope(
+      onWillPop: () {
+        debugPrint('willPopScope çalisti');
+        _rasgeleSayi = Random().nextInt(100);
+        Navigator.pop(context, _rasgeleSayi);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('RedPage'), backgroundColor: Colors.red,
+          automaticallyImplyLeading:
+              true, //sayfanın sol üstündeki geri dön buttonunu aç kapa yapıyor
         ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'RedPage',
+                style: TextStyle(fontSize: 24),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    _rasgeleSayi = Random().nextInt(100);
+                    print('Random Sayi $_rasgeleSayi');
+                    Navigator.of(context).pop(_rasgeleSayi);
+                  },
+                  child: Text(' Geri dön')),
+
+                              ElevatedButton(
+                onPressed: () async {
+                  if(Navigator.canPop(context)){
+                    debugPrint('evet pop olabilir');
+                  }else{
+                    debugPrint('Hayir pop olamaz');
+                  }
+                },
+                child: Text('Maybe Pop Kullanimi'),
+                style: ElevatedButton.styleFrom(primary: Colors.red.shade300)),
+            ],
+          ),
+        ),
+        // ),
       ),
     );
   }
